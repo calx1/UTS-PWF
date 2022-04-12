f0c507970114cf66462c86703f6d466ae684d5be
calx1 <103367228+calx1@users.noreply.github.com>
2022-04-12 23:33:04 +0700

    Create Product.php

---
+++ Product.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    // mendefinisikan tabel products
+    protected $table = 'products';
+
+    // mempersilahkan inputan mana saja yang bisa diinput langsung oleh user
+    protected $fillable = ['name','price','type','expired_at'];
+}
